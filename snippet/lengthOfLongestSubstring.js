9b158a3b4dbcd812044897e5c09d46229b9029a3
ArashSarikhani <69412956+ArashSarikhani@users.noreply.github.com>
2025-05-07 00:52:22 +0400

    lengthOfLongestSubstring

--- snippet/lengthOfLongestSubstring.js
+++ snippet/lengthOfLongestSubstring.js
@@ -5,39 +5,18 @@
 //output: 3
 //explanation: the longest substring without repeating characters is "abc", which the length is 3.
 function lengthOfLongestSubstring(s) {
-  return s.split("").reduce((acc, curr, i) => {
-    if (acc.indexOf(curr) === -1) {
-      acc.push(curr);
-    } else {
-      acc = acc.slice(acc.indexOf(curr) + 1);
-      acc.push(curr);
-    }
-    return acc.length > acc ? acc : acc;
-  }, []).length;
+  return s.split("").reduce(
+    ({ window, max }, char) => {
+      const index = window.indexOf(char);
+      if (index !== -1) {
+        window = window.slice(index + 1);
+      }
+      window.push(char);
+      return {
+        window,
+        max: Math.max(max, window.length),
+      };
+    },
+    { window: [], max: 0 }
+  ).max;
 }
-
-//without reduce function
-function lengthOfLongestSubstring(s) {
-  let longest = 0;
-  let current = 0;
-  let seen = {};
-  for (let i = 0; i < s.length; i++) {
-    let letter = s[i];
-    if (seen[letter]) {
-      current = Math.max(current, seen[letter]);
-    }
-    seen[letter] = i + 1;
-    longest = Math.max(longest, i - current + 1);
-  }
-  return longest;
-}
-
-const subStrOfUniqueChar = (str) =>
-  [...str].reduce((p, c) =>
-    p.includes(c) ? ((p += c), p.substr(p.indexOf(c) + 1)) : (p += c)
-  );
-
-
-  isSubstring = (str1, str2) => {
-    return str1.includes(str2);
-  }
